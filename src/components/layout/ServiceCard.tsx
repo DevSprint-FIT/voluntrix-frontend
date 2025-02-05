1309765fecfcba0c7d9ac067c29099c14ed054f3
Ishan Hansaka Silva <ishanhansakasilva@gmail.com>
2025-02-06 01:45:31 +0530

    feat: merge service section implementation into home (#2)
    * feat: add ServiceCard component
    
    * feat: setup ServiceSection with useState and useEffect for carousel timing
    
    * feat: add first service data
    
    * feat: add second service data
    
    * feat: add third service data
    
    * feat: add navigation dots to control active service slide
    
    * chore: update styles for ServiceCard and ServiceSection components
    
    * style: update font styles in components
    
    * refactor: remove a component from layout.tsx
    
    ---------
    
    Co-authored-by: Parindya2 <168643692+Parindya2@users.noreply.github.com>

---
+++ src/components/layout/ServiceCard.tsx
@@ -0,0 +1,28 @@
+import { ReactNode } from "react";
+
+interface ServiceCardProps {
+  imageUrl: string;
+  header: ReactNode;
+  paragraph: string;
+  end: string;
+}
+  
+
+const ServiceCard: React.FC<ServiceCardProps> = ({ imageUrl, header, paragraph, end }) => {
+  return (
+    <div className="flex flex-col md:flex-row items-center justify-center w-full min-h-[400px] text-center px-6 gap-8">
+      <img src={imageUrl} alt="Service" className="w-1/2" />
+      <div className="md:w-1/2 text-left">
+        <h2 className="text-3xl text-shark-900 font-primary font-bold">{header}</h2>
+        <p className="mt-2 text-gray-600 font-secondary-300">{paragraph}</p>
+        <div className="mt-4">
+          <a href="#" className="text-verdant-600 font-semibold hover:underline flex items-center gap-1">
+            {end} <span className="text-lg">→</span>
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default ServiceCard;
