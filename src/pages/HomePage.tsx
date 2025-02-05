0c41d1a99173c5d916963f3e6c328a9c2a9c9b6c
Ishan Hansaka Silva <ishanhansakasilva@gmail.com>
2025-02-06 01:29:13 +0530

    feat: create homepage

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,9 @@
+import EventSection from "@/components/layout/EventSection";
+
+export default function HomePage() {
+  return (
+    <div className="flex flex-col justify-center items-start flex-shrink-0">
+        <EventSection />
+    </div>
+  )
+}
