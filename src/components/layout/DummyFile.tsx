6a8186cf9446069771fc53ef366e1d73dde314ee
hadithya <hhadithya34@gmail.com>
2025-02-05 09:41:12 +0530

    chore: added new dummy file to show the folder structure

---
+++ src/components/layout/DummyFile.tsx
@@ -0,0 +1,8 @@
+// Desc: Don't confuse with the name, this is a dummy file to show the structure of the project.
+export default function Home() {
+    return ( 
+        <div className = "flex flex-col items-center justify-center min-h-screen text-center" >
+            <h1 className="text-xl" >This is a dummy text </h1>
+        </div>
+    );
+}
