65448b9c170852cb758857509799389cc4b385a0
Ishan Hansaka Silva <ishanhansakasilva@gmail.com>
2025-02-06 00:08:12 +0530

    chore: change react to progress

--- src/components/layout/EventCard.tsx
+++ src/components/layout/EventCard.tsx
@@ -2,7 +2,7 @@
 
 import Image from "next/image";
 import { useState } from "react";
-import { Progress } from "@heroui/react";
+import { Progress } from "@heroui/progress";
 
 export default function EventCard({ donation }: { donation: boolean }) {
   const [isSaved, setIsSaved] = useState(false);
