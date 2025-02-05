b5892b44c1ab8d0fec9a837e38594d69078c007c
Sanidi-Sisara <sanidi1113@gmail.com>
2025-02-06 02:54:25 +0530

    style: update navbar layout and design

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -1,26 +1,27 @@
 import Image from "next/image";
-import Button from "./Button";
 import Link from "next/link";
 
 export default function Navbar() {
   return (
-    <nav className="flex items-center justify-between px-8 py-4 bg-white shadow-md">
+    <nav className="flex items-center justify-between px-32 py-6 shadow-md">
       {/* Logo */}
-      <div className="flex items-center space-x-2">
+      <div className="flex items-center space-x-3">
         <Image
           src="/images/logo.svg"
           alt="Voluntrix Logo"
           width={40}
           height={40}
         />
-        <span className="text-xl font-bold text-verdant-600">Voluntrix</span>
+        <span className="text-2xl font-bold font-primary text-shark-950 tracking-widest">
+          Voluntrix
+        </span>
       </div>
 
       {/* Navigation Links */}
-      <div className="flex space-x-6 text-shark-800 font-primary">
+      <div className="flex space-x-12 text-shark-950 text-lg font-primary tracking-wider font-medium">
         <Link
           href="#"
-          className="relative text-verdant-600 font-semibold after:absolute after:-bottom-1 after:left-0 after:w-full after:h-0.5 after:bg-verdant-600"
+          className="relative after:absolute after:-bottom-1 after:left-0 after:w-full after:h-1 after:bg-verdant-600"
         >
           Home
         </Link>
@@ -39,10 +40,14 @@ export default function Navbar() {
       </div>
 
       {/* Login & Sign Up Buttons */}
-      {/* <div className="flex space-x-4">
-        <Button label="Login" variant="secondary" />
-        <Button label="Sign Up" variant="primary" />
-      </div> */}
+      <div className="flex space-x-4">
+        <button className="text-lg text-shark-950 font-primary px-5 py-2 border-0 tracking-widest font-medium">
+          Login
+        </button>
+        <button className="bg-verdant-600 text-white font-primary px-6 py-0 rounded-lg shadow-md tracking-widest">
+          Sign Up
+        </button>
+      </div>
     </nav>
   );
 }
