0984ca4b3b81a7afb9fa5dfa36a0155fc637183a
Ishan Hansaka Silva <ishanhansakasilva@gmail.com>
2025-02-06 02:48:08 +0530

    feat: add new volunteer section

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,11 +1,13 @@
 import EventSection from "@/components/layout/EventSection";
 import ServiceSection from "@/components/layout/ServiceSection";
+import VolunteerSection from "@/components/layout/VolunteerSection";
 
 export default function HomePage() {
   return (
     <div className="flex flex-col justify-center items-start flex-shrink-0 gap-[200px]">
       <ServiceSection />
       <EventSection />
+      <VolunteerSection />
     </div>
   );
 }
