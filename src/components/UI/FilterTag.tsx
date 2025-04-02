e446dc451f7aee31f9463480bcf676865a83395e
Ishan Hansaka <ishanhansakasilva@gmail.com>
2025-04-02 17:44:42 +0530

    chore: change styles

--- src/components/UI/FilterTag.tsx
+++ src/components/UI/FilterTag.tsx
@@ -6,8 +6,8 @@ interface FilterProps {
 
 export default function FilterTag({ name }: FilterProps) {
   return (
-    <div className="w-[130px] h-[44px] flex justify-center items-center bg-shark-50 rounded-[20px] gap-2">
-      <p className="font-secondary font-medium text-shark-950 text-[16px]">
+    <div className="px-3 py-2 flex justify-center items-center bg-shark-50 rounded-[20px] gap-2">
+      <p className="font-secondary text-shark-950 text-sm">
         {name}
       </p>
       <Image
