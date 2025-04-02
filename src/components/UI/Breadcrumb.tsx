6b65f67c067e25cac950010010f0e76bf4a6e266
Ishan Hansaka <ishanhansakasilva@gmail.com>
2025-04-02 17:01:26 +0530

    chore: change size

--- src/components/UI/Breadcrumb.tsx
+++ src/components/UI/Breadcrumb.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Breadcrumb() {
   return (
     <div className="h-8 w-[1200px] flex items-center justify-start">
-      <p className="font-secondary font-medium text-shark-950 text-lg">
+      <p className="font-secondary font-medium text-shark-950 text-md">
         <Link href="#">Home</Link>
         <span className="text-shark-300"> / </span> 
         <Link href="#">Events</Link>
