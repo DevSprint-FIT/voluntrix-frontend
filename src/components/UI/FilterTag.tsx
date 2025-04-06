67d65c38fb78a8e951b3054707aaaf966733e562
Ishan Hansaka <ishanhansakasilva@gmail.com>
2025-04-06 21:54:50 +0530

    chore: remove props

--- src/components/UI/FilterTag.tsx
+++ src/components/UI/FilterTag.tsx
@@ -1,25 +1,13 @@
-import Image from 'next/image';
-
 interface FilterTagProps {
   name: string;
-  index: number;
-  onRemove: (index: number) => void;
 }
 
-export default function FilterTag({ name, index, onRemove }: FilterTagProps) {
+export default function FilterTag({ name }: FilterTagProps) {
   return (
-    <div className="px-3 py-2 flex justify-center items-center bg-shark-50 rounded-[20px] gap-2">
+    <div className="px-3 py-2 flex justify-center items-center bg-shark-100 rounded-[20px] gap-2">
       <p className="font-secondary text-shark-950 text-sm">
         {name}
       </p>
-      <Image
-        src="/icons/close-circle.svg"
-        width={24}
-        height={24}
-        alt="Search icon"
-        className="cursor-pointer"
-        onClick={() => onRemove(index)}
-      />
     </div>
   );
 }
