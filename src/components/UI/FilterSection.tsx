230608ff1d063669176da2b00a7f3c40137150ca
Ishan Hansaka <ishanhansakasilva@gmail.com>
2025-04-11 13:04:32 +0530

    fix: correct clossing funtion after clear filter form

--- src/components/UI/FilterSection.tsx
+++ src/components/UI/FilterSection.tsx
@@ -118,7 +118,6 @@ export default function FilterSection({
   const handleClear = () => {
     setLocalFilters(initialFilters);
     setFilters(initialFilters);
-    setIsFilterTabOpen(false);
   };
 
   useEffect(() => {
