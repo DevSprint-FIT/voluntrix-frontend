0f861bfcc00f7a3c1ca87ed6f1b7ffce83cba1ec
Ishan Hansaka <ishanhansakasilva@gmail.com>
2025-04-25 23:54:37 +0530

    chore: enhanced skeliton ui

--- src/components/UI/EventSkeleton.tsx
+++ src/components/UI/EventSkeleton.tsx
@@ -4,64 +4,35 @@ export default function EventSkeleton() {
   return (
     <>
       <Card
-        className="w-[310px] h-[460px] flex flex-col items-center justify-start bg-white shadow-lg gap-6"
+        className="w-[310px] h-[460px] flex flex-col items-center justify-start bg-white shadow-lg gap-6 p-4"
         radius="md"
       >
-        <Skeleton className="rounded-md">
-          <div className="w-[310px] h-[165px] rounded-t-md bg-defualt" />
-        </Skeleton>
-        <div className="flex flex-col w-[258px] gap-4">
+        <Skeleton className="w-full h-[165px] rounded-md" />
+
+        <div className="flex flex-col w-full gap-4">
           <div className="flex items-center justify-between w-full">
-            <Skeleton className="w-[200px] rounded-lg">
-              <div className="h-4 w-full rounded-lg bg-defualt" />
-            </Skeleton>
-            <Skeleton className="rounded-full">
-              <div className="w-6 h-6 rounded-full bg-defualt" />
-            </Skeleton>
+            <Skeleton className="w-2/3 h-4 rounded-lg" />
+            <Skeleton className="w-6 h-6 rounded-full" />
           </div>
-          <Skeleton className="w-[130px] rounded-lg">
-            <div className="h-3 w-full rounded-lg bg-defualt-300" />
-          </Skeleton>
+          <Skeleton className="w-1/3 h-3 rounded-lg" />
           <div className="flex flex-col gap-2 mt-2">
-            <Skeleton className="w-[250px] rounded-lg">
-              <div className="h-2 w-full rounded-lg bg-defualt-200" />
-            </Skeleton>
-            <Skeleton className="w-[250px] rounded-lg">
-              <div className="h-2 w-full rounded-lg bg-defualt-200" />
-            </Skeleton>
-            <Skeleton className="w-[250px] rounded-lg">
-              <div className="h-2 w-full rounded-lg bg-defualt-200" />
-            </Skeleton>
-            <Skeleton className="w-[110px] rounded-lg">
-              <div className="h-2 w-full rounded-lg bg-defualt-200" />
-            </Skeleton>
+            {[1, 2, 3].map((i) => (
+              <Skeleton key={i} className="w-full h-2 rounded-lg" />
+            ))}
+            <Skeleton className="w-1/3 h-2 rounded-lg" />
           </div>
-          <div className="flex justify-start gap-2 mt-2">
-            <Skeleton className="w-[90px] rounded-md">
-              <div className="h-5 w-full rounded-md bg-defualt-300" />
-            </Skeleton>
-            <Skeleton className="w-[90px] rounded-md">
-              <div className="h-5 w-full rounded-md bg-defualt-300" />
-            </Skeleton>
+          <div className="flex gap-2 mt-2">
+            <Skeleton className="w-[90px] h-5 rounded-md" />
+            <Skeleton className="w-[90px] h-5 rounded-md" />
           </div>
-          <div className="flex gap-2 items-center mt-4">
-            <Skeleton className="w-6 rounded-full">
-              <div className="h-6 w-full rounded-full bg-defualt" />
-            </Skeleton>
+          <div className="flex items-center gap-2 mt-4">
+            <Skeleton className="w-6 h-6 rounded-full" />
             <div className="flex flex-col gap-2">
-              <Skeleton className="w-[70px] rounded-lg">
-                <div className="h-2 w-full rounded-lg bg-defualt-200" />
-              </Skeleton>
-              <Skeleton className="w-[70px] rounded-lg">
-                <div className="h-2 w-full rounded-lg bg-defualt-200" />
-              </Skeleton>
+              <Skeleton className="w-[70px] h-2 rounded-lg" />
+              <Skeleton className="w-[70px] h-2 rounded-lg" />
             </div>
-            <Skeleton className="w-6 rounded-full ml-6">
-              <div className="w-full h-6 rounded-full bg-defualt" />
-            </Skeleton>
-            <Skeleton className="w-[70px] h-2 rounded-lg">
-              <div className="h-full w-full rounded-lg bg-defualt-200" />
-            </Skeleton>
+            <Skeleton className="w-6 h-6 rounded-full ml-6" />
+            <Skeleton className="w-[70px] h-2 rounded-lg" />
           </div>
         </div>
       </Card>
