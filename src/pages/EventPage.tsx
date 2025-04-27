97812538c97808c3939e22c092802f3c2e39db32
Ishan Hansaka <ishanhansakasilva@gmail.com>
2025-04-27 12:43:18 +0530

    feat: featch with endpoint to get vent by id and also add event skeleton to improve UI/UX

--- src/pages/EventPage.tsx
+++ src/pages/EventPage.tsx
@@ -1,29 +1,15 @@
+'use client';
+
 import FooterSection from '@/components/layout/FooterSection';
 import GoToTop from '@/components/UI/GoToTop';
 import Navbar from '@/components/UI/Navbar';
 import Event from '@/components/layout/events/Event';
 import EventSection from '@/components/layout/events/EventSection';
 import { EventType } from '@/types/EventType';
-
-const eventData: EventType = {
-  eventTitle: 'FIT Future Careers',
-  eventDescription:
-    'Body text for your whole article or post. We’ll put in some lorem ipsum to show how a filled-out page might look: .Excepteur efficient emerging, minim veniam anim aute carefully curated Ginza conversation exquisite perfect nostrud nisi intricate Content. Qui  international first-class nulla ut. Punctual adipisicing, essential lovely queen tempor eiusmod irure. ',
-  eventLocation: 'FIT Auditorium',
-  eventDate: '2025-11-20',
-  eventTime: '10:00:00',
-  eventImageUrl: '/images/DummyEvent2.png',
-  eventType: 'ONLINE',
-  eventVisibility: 'PRIVATE',
-  eventStatus: 'PENDING',
-  sponsorshipEnabled: true,
-  donationEnabled: true,
-  categories: [
-    { categoryId: 1, categoryName: 'environment' },
-    { categoryId: 3, categoryName: 'technology' },
-  ],
-  organizer: 'INTECS, UoM',
-};
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import EventSkeleton from '@/components/UI/EventSkeleton';
+import Breadcrumb from '@/components/UI/Breadcrumb';
 
 const sponsorData = {
   sponsorships: [
@@ -34,11 +20,48 @@ const sponsorData = {
   ],
 };
 
-export default function EventPage() {
+export default function EventPage(/*{ params }: { params: { eventId: number } }*/) {
+  const [event, setEvent] = useState<EventType | null>(null);
+  const [error, setError] = useState<string | null>(null);
+  const [loading, setLoading] = useState<boolean>(true);
+
+  useEffect(
+    () => {
+      const fetchEventById = async () => {
+        try {
+          const response = await axios.get<EventType>(
+            `http://localhost:8080/api/public/v1/events/1`
+          );
+          setEvent(response.data);
+        } catch (err: unknown) {
+          if (err instanceof Error) {
+            setError(err.message);
+          } else {
+            setError('Unexpected error occurred.');
+          }
+        } finally {
+          setLoading(false);
+        }
+      };
+
+      fetchEventById();
+    },
+    [
+      /*params.eventId*/
+    ]
+  );
+
   return (
     <div className="flex flex-col justify-center items-start flex-shrink-0">
       <Navbar />
-      <Event event={eventData} sponsor={sponsorData} />
+      <Breadcrumb />
+      {loading ? (
+        <EventSkeleton />
+      ) : error ? (
+        <div className="text-center w-full py-10 text-red-500"></div>
+      ) : event ? (
+        <Event event={event} sponsor={sponsorData} />
+      ) : null}
       <EventSection
         title={'Based on your browsing history'}
         subTitle={'Based on searches and prefernces'}
