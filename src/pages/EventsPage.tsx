8c675157d05a63f82ac0e4a9a253adc6fe7f0bb8
Ishan Hansaka <ishanhansakasilva@gmail.com>
2025-04-27 12:40:23 +0530

    chore: add breadcrumb without adding indside component

--- src/pages/EventsPage.tsx
+++ src/pages/EventsPage.tsx
@@ -1,6 +1,7 @@
 import EventSection from '@/components/layout/events/EventSection';
 import HeroSection from '@/components/layout/events/HeroSection';
 import FooterSection from '@/components/layout/FooterSection';
+import Breadcrumb from '@/components/UI/Breadcrumb';
 import GoToTop from '@/components/UI/GoToTop';
 import Navbar from '@/components/UI/Navbar';
 
@@ -8,6 +9,7 @@ export default function EventsPage() {
   return (
     <div className="flex flex-col justify-center items-start flex-shrink-0">
       <Navbar />
+      <Breadcrumb />
       <HeroSection />
       <EventSection
         title={'Trending events'}
