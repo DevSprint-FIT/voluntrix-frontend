2ef8474110c05a69f538ad9b3267868a77f9826f
Ishan Hansaka <ishanhansakasilva@gmail.com>
2025-04-27 12:39:40 +0530

    feat: create event skeleton for event page

--- src/components/UI/EventSkeleton.tsx
+++ src/components/UI/EventSkeleton.tsx
@@ -1,39 +1,64 @@
-import { Card, Skeleton } from '@heroui/react';
+import { Skeleton } from '@heroui/react';
 
 export default function EventSkeleton() {
   return (
-    <Card
-      className="w-[310px] h-[460px] flex flex-col items-center justify-start bg-white shadow-lg gap-3"
-      radius="md"
-    >
-      <Skeleton className="w-full h-[165px] rounded-md" />
-
-      <div className="flex flex-col w-full gap-4 p-4">
-        <div className="flex items-center justify-between w-full">
-          <Skeleton className="w-2/3 h-4 rounded-lg" />
-          <Skeleton className="w-6 h-6 rounded-full" />
-        </div>
-        <Skeleton className="w-1/3 h-3 rounded-lg" />
-        <div className="flex flex-col gap-2 mt-2">
-          {[1, 2, 3].map((i) => (
-            <Skeleton key={i} className="w-full h-2 rounded-lg" />
-          ))}
-          <Skeleton className="w-1/3 h-2 rounded-lg" />
-        </div>
-        <div className="flex gap-2 mt-2">
-          <Skeleton className="w-[90px] h-5 rounded-md" />
-          <Skeleton className="w-[90px] h-5 rounded-md" />
-        </div>
-        <div className="flex items-center gap-2 mt-4">
-          <Skeleton className="w-6 h-6 rounded-full" />
-          <div className="flex flex-col gap-2">
-            <Skeleton className="w-[70px] h-2 rounded-lg" />
-            <Skeleton className="w-[70px] h-2 rounded-lg" />
+    <div className="w-full flex items-start justify-center mb-[88px]">
+      <div className="w-[1200px] flex items-start justify-center">
+        <div className="mt-14 flex gap-20">
+          <div className="w-[522px] flex items-center justify-end">
+            <Skeleton className="w-[495px] h-[369px] rounded-lg" />
           </div>
-          <Skeleton className="w-6 h-6 rounded-full ml-6" />
-          <Skeleton className="w-[70px] h-2 rounded-lg" />
+          <div className="flex justify-start w-[545px]">
+            <div className="flex flex-col items-start gap-5">
+              <div className="flex flex-col gap-2">
+                <div className="flex w-[545px] items-start justify-between">
+                  <Skeleton className="w-[350px] h-8 rounded-full" />
+                  <Skeleton className="w-8 h-8 rounded-full" />
+                </div>
+                <div className="flex gap-2 items-center mt-4">
+                  <Skeleton className="w-10 h-10 rounded-full" />
+                  <Skeleton className="w-[120px] h-5 rounded-full mx-2" />
+                </div>
+              </div>
+              <div className="flex gap-6">
+                <div className="flex gap-2 items-center mt-1">
+                  <Skeleton className="w-7 h-7 rounded-full" />
+                  <div className="flex flex-col gap-2">
+                    <Skeleton className="w-[100px] h-2 rounded-lg" />
+                    <Skeleton className="w-[100px] h-2 rounded-lg" />
+                  </div>
+                </div>
+                <div className="flex gap-2 items-center">
+                  <Skeleton className="w-7 h-7 rounded-full" />
+                  <div className="flex flex-col gap-2">
+                    <Skeleton className="w-[100px] h-2 rounded-lg" />
+                    <Skeleton className="w-[100px] h-2 rounded-lg" />
+                  </div>
+                </div>
+                <div className="flex gap-2 items-center">
+                  <Skeleton className="w-7 h-7 rounded-full" />
+                  <Skeleton className="w-[100px] h-2 rounded-lg" />
+                </div>
+              </div>
+              <div className="flex flex-col gap-5 mt-4">
+                <div className="flex flex-col gap-2">
+                  <Skeleton className="w-[545px] h-2 rounded-lg" />
+                  <Skeleton className="w-[345px] h-2 rounded-lg" />
+                </div>
+                <div className="flex flex-col gap-2">
+                  <Skeleton className="w-[545px] h-2 rounded-lg" />
+                  <Skeleton className="w-[545px] h-2 rounded-lg" />
+                  <Skeleton className="w-[545px] h-2 rounded-lg" />
+                  <Skeleton className="w-[545px] h-2 rounded-lg" />
+                  <Skeleton className="w-[545px] h-2 rounded-lg" />
+                  <Skeleton className="w-[100px] h-2 rounded-lg" />
+                </div>
+              </div>
+              <Skeleton className="w-[180px] h-10 rounded-full mt-4" />
+            </div>
+          </div>
         </div>
       </div>
-    </Card>
+    </div>
   );
 }
