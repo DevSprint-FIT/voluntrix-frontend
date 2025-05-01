eb101ab7c3c233ca85d1197dd2816375db144036
Parindya2 <168643692+Parindya2@users.noreply.github.com>
2025-05-01 10:14:05 +0530

    feat(settings): implement UI for organization settings

---
+++ src/app/Organization/settings/page.tsx
@@ -0,0 +1,95 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { getOrganizationSettingsByUsername, OrganizationSettings } from "@/services/organizationSettingsService";
+import { useRouter } from "next/navigation";
+import { Button } from "@heroui/button";
+
+const SettingsPage = () => {
+  const [organization, setOrganization] = useState<OrganizationSettings | null>(null);
+  const [loading, setLoading] = useState(true);
+  const router = useRouter();
+
+  // Replace the username
+  const username = "IEEEUOM";
+
+  useEffect(() => {
+    const loadOrganization = async () => {
+      try {
+        const data = await getOrganizationSettingsByUsername(username);
+        setOrganization(data);
+      } catch (error) {
+        console.error("Failed to fetch organization", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    loadOrganization();
+  }, []);
+
+  if (loading) {
+    return (
+      <div className="flex items-center justify-center min-h-[50vh]">
+        <div className="w-10 h-10 border-4 border-shark-950 border-t-transparent rounded-full animate-spin" />
+      </div>
+    );
+  }
+  
+
+  if (!organization) {
+    return <div className="p-6">Organization not found.</div>;
+  }
+
+  return (
+    <div className="p-5">
+        <span className="text-shark-300 text">Organization / Settings</span>
+      <h1 className=" font-secondary font-bold mb-6 text-2xl mt-2">Settings</h1>
+
+      {/* Email Section */}
+      <div className="bg-shark-50 shadow rounded-2xl p-6 mb-6 pr-20 pl-10">
+        <div className="flex justify-between items-center mb-4">
+          <div>
+            <h2 className="font-secondary font-semibold text-xl">Your email address</h2>
+            <p className="mb-4">{organization.email}</p>
+            <Button className="rounded-full bg-shark-950 text-shark-50 font-primary">
+            Change email
+          </Button>
+          </div>
+          <div className="flex flex-col ">
+            <p className="font-secondary text-shark-950 font-medium">Your username</p>
+            <p className="font-secondary text-shark-600">@{organization.username.replace(/\s+/g, '')}</p>
+            <p className="font-secondary text-shark-950 mt-2 font-medium">Account Number</p>
+            <p className="font-secondary text-shark-600">{organization.accountNumber}</p>
+          </div>
+        </div>
+    </div>
+
+      {/* Phone Verification Section */}
+      <div className="bg-shark-50 shadow rounded-2xl p-6 mb-6 pr-20 pl-10">
+        <h2 className="font-secondary font-semibold text-xl mb-2">Phone Verification</h2>
+        <p className="mb-4">
+          {organization.isVerified
+            ? "Your account is verified."
+            : "Your account is not verified. Verifying your account with a phone number allows you to do more on Voluntrix, and helps prevent spam and other abuse."}
+        </p>
+        {!organization.isVerified && (
+          <Button className="rounded-full bg-shark-950 text-shark-50 font-primary">
+            Phone verify
+          </Button>
+        )}
+      </div>
+
+      {/* Danger Zone Section */}
+      <div className="bg-shark-50 shadow rounded-2xl p-6 pl-10">
+        <h2 className="font-secondary font-semibold text-xl text-red-600 mb-2">Danger Zone</h2>
+        <p className="mb-4  text-gray-500">Permanently delete your account and all data.</p>
+        <Button className="rounded-full bg-red-600 text-shark-50 font-primary">
+          Delete Account
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default SettingsPage;
