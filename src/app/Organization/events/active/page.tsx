ae067d005f3a000b80f8eb0b1dee5689dd10538a
Parindya2 <168643692+Parindya2@users.noreply.github.com>
2025-05-01 15:09:08 +0530

    add pages for active, completed and pending events

---
+++ src/app/Organization/events/active/page.tsx
@@ -0,0 +1,64 @@
+// src/app/organization/events/active/page.tsx
+
+"use client";
+
+import { useEffect, useState } from "react";
+import Table, { Column } from "@/components/UI/Table";
+import { Event, getEventsByOrgId } from "@/services/eventTableService";
+import { Loader2 } from "lucide-react";
+
+
+export default function ActiveEventsPage() {
+  const [events, setEvents] = useState<Event[]>([]);
+  const [loading, setLoading] = useState(true);
+
+  const orgId = 4; // Replace this with actual org ID 
+
+  useEffect(() => {
+    const fetchEvents = async () => {
+      try {
+        const data = await getEventsByOrgId(orgId, "ACTIVE");
+        setEvents(data);
+      } catch (error) {
+        console.error("Failed to load active events", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchEvents();
+  }, []);
+
+  const columns: Column<Event>[] = [
+    { header: "Event Name", accessor: "eventTitle",
+      cell: (value, row) => (
+        <div className="flex flex-col">
+          <span className="font-bold">{value}</span>
+          <span className="text-xs text-shark-500">#{row.eventId}</span> {/* show ID under title */}
+        </div>
+      )
+     },
+    { header: "Date", accessor: "eventDate" },
+    { header: "Location", accessor: "eventLocation" },
+    { header: "Status", accessor: "eventStatus",
+      cell: () => (
+        <span className=" rounded-full bg-verdant-100 px-3 py-1 text-xs  text-verdant-600">
+          Active
+        </span>
+      ),
+     },
+  ];
+
+return (
+    <div className="p-4">
+      <h1 className="text-2xl font-bold mb-4">Active Events</h1>
+      {loading ? (
+      <div className="flex justify-center items-center">
+      <Loader2 className="h-8 w-8 text-verdant-600 animate-spin" />
+    </div>
+      ) : (
+     <Table columns={columns} data={events} />
+     )}
+   </div>
+  );
+}
