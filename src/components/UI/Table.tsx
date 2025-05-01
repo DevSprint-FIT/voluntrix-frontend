36381e5a66d14edc7649d12c1becc2f16d1c4505
Parindya2 <168643692+Parindya2@users.noreply.github.com>
2025-05-01 08:36:58 +0530

    feat(table): create generic table component

--- src/components/UI/Table.tsx
+++ src/components/UI/Table.tsx
@@ -14,23 +14,25 @@ type TableProps<T> = {
 export default function Table<T extends Record<string, any>>({ columns, data }: TableProps<T>) {
   return (
     <div className="overflow-x-auto rounded-xl shadow-sm">
-      <table className="min-w-full divide-y divide-gray-200 bg-white text-sm">
-        <thead className="bg-gray-50">
+      <table className="min-w-full divide-y divide-shark-100 bg-white text-sm border-separate border-spacing-y-2">
+        <thead className="bg-white">
           <tr>
             {columns.map((col, idx) => (
-              <th key={idx} className="px-6 py-3 text-left font-medium text-gray-600">
+              <th key={idx} className="px-6 py-3 text-left font-secondary text-shark-600">
                 {col.header}
               </th>
             ))}
           </tr>
         </thead>
-        <tbody className="divide-y divide-gray-100">
+        <tbody>
           {data.map((row, i) => (
-            <tr key={i} className="hover:bg-gray-50">
+            <tr key={i}>
               {columns.map((col, j) => {
                 const cellValue = row[col.accessor];
                 return (
-                  <td key={j} className="px-6 py-4 text-gray-800">
+                  <td key={j} 
+                  style={{ backgroundColor: "#F8F8F8" }}
+                  className="px-6 py-4 text-shark-900 font-secondary font-bold ">
                     {col.cell ? col.cell(cellValue, row) : String(cellValue)}
                   </td>
                 );
