acbc7965810fbb0889975de9e1bd93c70fea86c5
Parindya2 <168643692+Parindya2@users.noreply.github.com>
2025-05-01 08:40:25 +0530

    feat(card): add EventStatusCard component

--- src/components/UI/EventStatusCard.tsx
+++ src/components/UI/EventStatusCard.tsx
@@ -1,29 +1,33 @@
-// components/UI/EventStatusCard.tsx
 import React from "react";
 import { BarChart } from "lucide-react";
 
 interface EventStatusCardProps {
-  count: number;
+  count?: number;
   label: string;
   subtext: string;
+  loading?: boolean; 
 }
 
-const EventStatusCard: React.FC<EventStatusCardProps> = ({ count, label, subtext }) => {
+const EventStatusCard: React.FC<EventStatusCardProps> = ({ count, label, subtext, loading }) => {
   return (
-    <div className="bg-shark-50  rounded-xl p-4  min-w-[300px] flex-grow-0 flex-shrink-0 relative">
-          
-          
-          <div className="absolute right-6 top-1/2 transform -translate-y-1/2 bg-verdant-100 rounded-full p-2">
-          <BarChart
-             size={20}
-             className="text-verdant-700 "
-          />
-          </div>
-          
+    <div
+      style={{ backgroundColor: "#F8F8F8" }}
+      className="rounded-xl p-6 min-w-[24rem] flex-grow-0 flex-shrink-0 relative"
+    >
+      <div className="absolute right-6 top-1/2 transform -translate-y-1/2 bg-verdant-100 rounded-full p-2">
+        <BarChart size={24} className="text-verdant-700" />
+      </div>
 
       <h3 className="font-secondary text-shark-950 font-medium">{label}</h3>
-      <p className="text-2xl font-bold text-verdant-700">{count}</p>
-      <p className="font-secondary text-shark-300 text-xs font-medium">{subtext}</p>
+      <p className="text-2xl font-bold text-verdant-600">
+        {loading ? (
+        <div className="w-6 h-6 border-4 border-verdant-600 border-t-transparent rounded-full animate-spin mx-auto" />
+        ) : (
+        count
+        )}
+     </p>
+
+      <p className="font-secondary text-shark-300">{subtext}</p>
     </div>
   );
 };
