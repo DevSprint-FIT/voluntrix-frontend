457cb0f7f4263fb735f9609acd3439ea45f093ea
Parindya2 <168643692+Parindya2@users.noreply.github.com>
2025-05-02 18:16:29 +0530

    feat(component): add MetricCard component for displaying metrics

---
+++ src/components/UI/MetricCard.tsx
@@ -0,0 +1,26 @@
+"use client"
+
+import React from "react"
+
+interface MetricCardProps {
+    title: string;
+    value: number;
+    percentageChange: string;
+    icon: React.ReactNode;
+}
+
+const MetricCard:React.FC<MetricCardProps> = ({ title, value, percentageChange, icon}) => {
+    return(
+        <div className="bg-[#F8F8F8] rounded-xl p-4 shadow-md flex justify-between items-center mb-3">
+            <div className="flex flex-col gap-1">
+            <div className="text-shark-900 font-bold">{title}</div>
+            <div className="text-2xl font-bold text-verdant-500">{value}</div>
+            <div className="text-sm text-shark-400">{percentageChange}</div>
+            </div>
+            
+            <div className="text-verdant-500 mt-2">{icon}</div>
+        </div>
+    );
+};
+
+export default MetricCard;
