a29621a2e8b119fb0856a6f83095052972df4716
Parindya2 <168643692+Parindya2@users.noreply.github.com>
2025-05-05 14:19:19 +0530

    types: define Post, Organization and Api Response interfaces

---
+++ src/services/types.ts
@@ -0,0 +1,29 @@
+export type MediaType = "TEXT" | "IMAGE" | "VIDEO";
+
+export interface Post {
+  id: number;
+  content: string;
+  mediaUrl?: string;
+  mediaType: MediaType;
+  mediaSizeInBytes?: number;
+  timeAgo?: string;
+  createdAt: string;
+  updatedAt: string;
+  organizationName: string;
+  organizationImageUrl: string;
+  impressions?: number;
+  shares?: number;
+}
+
+export interface Organization {
+  id: number;
+  name: string;
+  imageUrl?: string;
+  followerCount: number;
+  institute: string;
+}
+
+export interface ApiResponse<T> {
+  message: string;
+  data: T;
+}
