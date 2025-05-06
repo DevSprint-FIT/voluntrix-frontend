8676e55e5cd06e2ccceb1feeaa69acb1c77e5dea
Sanidi-Sisara <sanidi1113@gmail.com>
2025-05-06 20:56:11 +0530

    feat: add navbar to the volunteer layout

--- src/app/Volunteer/layout.tsx
+++ src/app/Volunteer/layout.tsx
@@ -9,7 +9,7 @@ export default function VolunteerLayout({
   return (
     <div className="flex">
       <Sidebar />
-      <div className="flex-1 p-6 ml-60">{children}</div>
+      <div className="flex-1 p-6 ml-5">{children}</div>
     </div>
   );
 }
