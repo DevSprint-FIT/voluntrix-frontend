a38fb4a5e84b6f400b07ef99757b68a165cb3124
Parindya2 <168643692+Parindya2@users.noreply.github.com>
2025-05-09 22:38:37 +0530

    style(metric-card): update spacing and color styles

--- src/components/UI/MetricCard.tsx
+++ src/components/UI/MetricCard.tsx
@@ -11,14 +11,14 @@ interface MetricCardProps {
 
 const MetricCard:React.FC<MetricCardProps> = ({ title, value, percentageChange, icon}) => {
     return(
-        <div className="bg-[#F8F8F8] rounded-xl p-4 shadow-md flex justify-between items-center mb-3 ml-4">
-            <div className="flex flex-col gap-1">
-            <div className="text-shark-900 font-bold">{title}</div>
-            <div className="text-2xl font-bold text-verdant-500">{value}</div>
-            <div className="text-sm text-shark-400">{percentageChange}</div>
+        <div className="bg-[#F8F8F8] rounded-xl p-4 pr-2 shadow-sm flex justify-between items-center mb-3 ml-4 ">
+            <div className="flex flex-col ">
+               <div className="text-shark-900 font-medium">{title}</div>
+               <div className="text-2xl font-bold text-verdant-600">{value}</div>
+               <div className="text-sm text-shark-300">{percentageChange}</div>
             </div>
             
-            <div className="text-verdant-500 mt-2">{icon}</div>
+            <div className="text-verdant-200 mt-2 ">{icon}</div>
         </div>
     );
 };
