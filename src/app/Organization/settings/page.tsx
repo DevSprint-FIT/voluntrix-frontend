173ab16133f7641f7bbf64abe22b2cf5af19a3ed
Parindya2 <168643692+Parindya2@users.noreply.github.com>
2025-05-10 20:12:16 +0530

    feat(settings): add phone verification modal to settings page

--- src/app/Organization/settings/page.tsx
+++ src/app/Organization/settings/page.tsx
@@ -1,15 +1,20 @@
 "use client";
 
 import { useEffect, useState } from "react";
-import { getOrganizationSettingsByUsername, OrganizationSettings } from "@/services/organizationSettingsService";
+import {
+  getOrganizationSettingsByUsername,
+  OrganizationSettings,
+} from "@/services/organizationSettingsService";
 import { useRouter } from "next/navigation";
 import { Button } from "@heroui/button";
+import PhoneVerificationModal from "@/components/UI/PhoneVerification";
 
 const SettingsPage = () => {
   const [organization, setOrganization] = useState<OrganizationSettings | null>(null);
   const [loading, setLoading] = useState(true);
+  const [isModalOpen, setIsModalOpen] = useState(false);
   const router = useRouter();
-  const username = "IEEEUOM"; 
+  const username = "IEEESLIT";
 
   useEffect(() => {
     const loadOrganization = async () => {
@@ -35,24 +40,32 @@ const SettingsPage = () => {
         <div className="flex justify-between items-center mb-4">
           <div>
             <h2 className="font-secondary font-semibold text-xl">Your email address</h2>
-            <p className="mb-4 text-shark-700">
-              {organization?.email || <div className="h-4 w-32 bg-shark-100 rounded animate-pulse"></div>}
-            </p>
+            <div className="mb-4 text-shark-700">
+              {organization?.email || (
+                <div className="h-4 w-32 bg-shark-100 rounded animate-pulse"></div>
+              )}
+            </div>
             <Button className="rounded-full bg-shark-950 text-shark-50 font-primary">
               Change email
             </Button>
           </div>
+
           <div className="flex flex-col">
-            <p className="font-secondary text-shark-950 font-medium">Your username</p>
-            <p className="font-secondary text-shark-600">
-              {organization
-                ? `@${organization.username.replace(/\s+/g, "")}`
-                : <div className="h-4 w-24 bg-shark-100 rounded animate-pulse" />}
-            </p>
-            <p className="font-secondary text-shark-950 mt-2 font-medium">Account Number</p>
-            <p className="font-secondary text-shark-600">
-              {organization?.accountNumber || <div className="h-4 w-28 bg-shark-100 rounded animate-pulse"></div>}
-            </p>
+            <div className="font-secondary text-shark-950 font-medium">Your username</div>
+            <div className="font-secondary text-shark-600">
+              {organization ? (
+                `@${organization.username.replace(/\s+/g, "")}`
+              ) : (
+                <div className="h-4 w-24 bg-shark-100 rounded animate-pulse" />
+              )}
+            </div>
+
+            <div className="font-secondary text-shark-950 mt-2 font-medium">Account Number</div>
+            <div className="font-secondary text-shark-600">
+              {organization?.accountNumber || (
+                <div className="h-4 w-28 bg-shark-100 rounded animate-pulse"></div>
+              )}
+            </div>
           </div>
         </div>
       </div>
@@ -60,15 +73,20 @@ const SettingsPage = () => {
       {/* Phone Verification Section */}
       <div className="bg-shark-50 shadow rounded-2xl p-6 mb-6 pr-20 pl-10">
         <h2 className="font-secondary font-semibold text-xl mb-2">Phone Verification</h2>
-        <p className="mb-4 text-shark-700">
-          {organization?.isVerified
-            ? "Your account is verified."
-            : organization !== null
-            ? "Your account is not verified. Verifying your account with a phone number allows you to do more on Voluntrix, and helps prevent spam and other abuse."
-            : <div className="h-4 w-full max-w-md bg-shark-100 rounded animate-pulse"></div>}
-        </p>
+        <div className="mb-4 text-shark-700">
+          {organization?.isVerified ? (
+            "Your account is verified."
+          ) : organization !== null ? (
+            "Your account is not verified. Verifying your account with a phone number allows you to do more on Voluntrix, and helps prevent spam and other abuse."
+          ) : (
+            <div className="h-4 w-full max-w-md bg-shark-100 rounded animate-pulse"></div>
+          )}
+        </div>
+
         {organization && !organization.isVerified && (
-          <Button className="rounded-full bg-shark-950 text-shark-50 font-primary">
+          <Button 
+            onPress = {() => setIsModalOpen(true)}
+            className="rounded-full bg-shark-950 text-shark-50 font-primary">
             Phone verify
           </Button>
         )}
@@ -77,11 +95,17 @@ const SettingsPage = () => {
       {/* Danger Zone Section */}
       <div className="bg-shark-50 shadow rounded-2xl p-6 pl-10">
         <h2 className="font-secondary font-semibold text-xl text-red-600 mb-2">Danger Zone</h2>
-        <p className="mb-4 text-gray-500">Permanently delete your account and all data.</p>
+        <div className="mb-4 text-gray-500">Permanently delete your account and all data.</div>
         <Button className="rounded-full bg-red-600 text-shark-50 font-primary">
           Delete Account
         </Button>
       </div>
+
+      {/* Phone Verification Modal */}
+      <PhoneVerificationModal
+      isOpen={isModalOpen}
+      onClose={() => setIsModalOpen(false)}
+    />
     </div>
   );
 };
