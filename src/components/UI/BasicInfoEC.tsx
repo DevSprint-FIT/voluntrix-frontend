83206f88eb2f399458aba9ff9016c166fd4fb936
Ishan Hansaka <ishanhansakasilva@gmail.com>
2025-07-15 17:59:33 +0530

    chore: pass all data to parent component

--- src/components/UI/BasicInfoEC.tsx
+++ src/components/UI/BasicInfoEC.tsx
@@ -1,44 +1,54 @@
+import { EventCreateData } from '@/types/EventCreateData';
 import { Select, SelectItem } from '@heroui/react';
 import { useState, useEffect } from 'react';
 
 interface Props {
-  onValidityChange?: (valid: boolean) => void;
+  data: EventCreateData;
+  onChange: (changes: Partial<EventCreateData>) => void;
+  onValidityChange: (valid: boolean) => void;
 }
 
 const eventTypes = [
-  { key: 'private', label: 'Private' },
-  { key: 'public', label: 'Public' },
+  { key: 'PRIVATE', label: 'Private' },
+  { key: 'PUBLIC', label: 'Public' },
 ];
 
-export default function BasicInfoEC({ onValidityChange }: Props) {
-  const [title, setTitle] = useState('');
-  const [location, setLocation] = useState('');
-  const [description, setDesc] = useState('');
-  const [eventType, setEventType] = useState<string | null>(null);
-  const [startDate, setStartDate] = useState('');
-  const [endDate, setEndDate] = useState('');
-  const [time, setTime] = useState('');
-  const [file, setFile] = useState<File | null>(null);
-  const [imageMessage, setImagemessage] = useState('');
+const categories = [
+  { categoryId: 1, categoryName: 'environment' },
+  { categoryId: 2, categoryName: 'sports' },
+  { categoryId: 3, categoryName: 'technology' },
+  { categoryId: 4, categoryName: 'charity' },
+  { categoryId: 5, categoryName: 'education' },
+];
+
+export default function BasicInfoEC({
+  data,
+  onChange,
+  onValidityChange,
+}: Props) {
+  const [imageMessage, setImageMessage] = useState('');
 
   useEffect(() => {
     const valid =
-      title.trim() &&
-      location.trim() &&
-      description.trim() &&
-      eventType &&
-      startDate &&
-      file;
-    onValidityChange?.(!!valid);
-  }, [
-    title,
-    location,
-    description,
-    eventType,
-    startDate,
-    file,
-    onValidityChange,
-  ]);
+      data.eventTitle.trim() &&
+      data.eventLocation.trim() &&
+      data.eventDescription.trim() &&
+      data.eventType &&
+      data.eventStartDate &&
+      data.eventImageUrl;
+
+    onValidityChange(!!valid);
+  }, [data, onValidityChange]);
+
+  const handleCategoryChange = (
+    category: { categoryId: number; categoryName: string },
+    checked: boolean
+  ) => {
+    const newCategories = checked
+      ? [...data.categories, category]
+      : data.categories.filter((cat) => cat.categoryId !== category.categoryId);
+    onChange({ categories: newCategories });
+  };
 
   return (
     <div className="flex flex-col">
@@ -48,8 +58,8 @@ export default function BasicInfoEC({ onValidityChange }: Props) {
           <input
             type="text"
             className="border-[2px] border-shark-300 text-shark-950 pl-2 py-1 rounded-lg w-[350px] placeholder:text-shark-300"
-            value={title}
-            onChange={(e) => setTitle(e.target.value)}
+            value={data.eventTitle}
+            onChange={(e) => onChange({ eventTitle: e.target.value })}
             placeholder="Enter event title"
           />
         </label>
@@ -59,8 +69,8 @@ export default function BasicInfoEC({ onValidityChange }: Props) {
             type="text"
             className="border-[2px] border-shark-300 text-shark-950 pl-2 py-1 rounded-lg w-[350px] placeholder:text-shark-300"
             placeholder="Enter event location"
-            value={location}
-            onChange={(e) => setLocation(e.target.value)}
+            value={data.eventLocation}
+            onChange={(e) => onChange({ eventLocation: e.target.value })}
           />
         </label>
       </div>
@@ -68,15 +78,15 @@ export default function BasicInfoEC({ onValidityChange }: Props) {
         <label className="font-secondary font-medium text-shark-950 text-[15px]">
           Event Description*
           <textarea
-            value={description}
-            onChange={(e) => setDesc(e.target.value)}
+            value={data.eventDescription}
+            onChange={(e) => onChange({ eventDescription: e.target.value })}
             maxLength={150}
             placeholder="Describe your event"
             className="resize-none border-[2px] border-shark-300 text-shark-950 pl-2 pt-1 rounded-lg w-full placeholder:text-shark-300 h-[80px]"
           />
         </label>
         <div className="w-full text-right font-secondary font-normal text-shark-800 text-[11px]">
-          {description.length}/150
+          {data.eventDescription.length}/150
         </div>
       </div>
       <div className="flex gap-8 mt-2">
@@ -86,8 +96,8 @@ export default function BasicInfoEC({ onValidityChange }: Props) {
             placeholder="Select type"
             variant="bordered"
             size="sm"
-            value={eventType ?? undefined}
-            onChange={(e) => setEventType(e.target.value)}
+            value={data.eventType || ''}
+            onChange={(e) => onChange({ eventType: e.target.value })}
             className="w-[170px] rounded-2xl text-shark-300 border-shark-300"
             classNames={{
               base: 'border-shark-300',
@@ -110,8 +120,8 @@ export default function BasicInfoEC({ onValidityChange }: Props) {
           <input
             type="date"
             className="w-[141px] h-8 border-2 border-shark-300 rounded-lg text-shark-300"
-            value={startDate}
-            onChange={(e) => setStartDate(e.target.value)}
+            value={data.eventStartDate}
+            onChange={(e) => onChange({ eventStartDate: e.target.value })}
           />
         </label>
         <label className="flex flex-col font-secondary font-medium text-shark-950 text-[15px]">
@@ -119,8 +129,8 @@ export default function BasicInfoEC({ onValidityChange }: Props) {
           <input
             type="date"
             className="w-[141px] h-8 border-2 border-shark-300 rounded-lg text-shark-300"
-            value={endDate}
-            onChange={(e) => setEndDate(e.target.value)}
+            value={data.eventEndDate}
+            onChange={(e) => onChange({ eventEndDate: e.target.value })}
           />
         </label>
         <label className="flex flex-col font-secondary font-medium text-shark-950 text-[15px]">
@@ -128,8 +138,8 @@ export default function BasicInfoEC({ onValidityChange }: Props) {
           <input
             type="time"
             className="px-4 rounded-lg border-[2px] h-8 border-shark-300 text-shark-500 placeholder:text-shark-400 focus:ring-shark-400 focus:border-shark-400"
-            value={time}
-            onChange={(e) => setTime(e.target.value)}
+            value={data.eventTime}
+            onChange={(e) => onChange({ eventTime: e.target.value })}
           />
         </label>
       </div>
@@ -137,51 +147,21 @@ export default function BasicInfoEC({ onValidityChange }: Props) {
         Event Category
       </div>
       <div className="text-[15px] font-secondary text-shark-950 font-normal flex gap-5">
-        <label className="flex items-center gap-2">
-          <input
-            type="checkbox"
-            name="categories"
-            value="environment"
-            className="accent-verdant-600"
-          />
-          Environment
-        </label>
-        <label className="flex items-center gap-2">
-          <input
-            className="accent-verdant-600"
-            type="checkbox"
-            name="categories"
-            value="sport"
-          />
-          Sport
-        </label>
-        <label className="flex items-center gap-2">
-          <input
-            className="accent-verdant-600"
-            type="checkbox"
-            name="categories"
-            value="technology"
-          />
-          Technology
-        </label>
-        <label className="flex items-center gap-2">
-          <input
-            className="accent-verdant-600"
-            type="checkbox"
-            name="categories"
-            value="charity"
-          />
-          Charity
-        </label>
-        <label className="flex items-center gap-2">
-          <input
-            className="accent-verdant-600"
-            type="checkbox"
-            name="categories"
-            value="education"
-          />
-          Education
-        </label>
+        {categories.map((category) => (
+          <label key={category.categoryId} className="flex items-center gap-2">
+            <input
+              type="checkbox"
+              value={category.categoryId}
+              className="accent-verdant-600"
+              checked={data.categories.some(
+                (cat) => cat.categoryId === category.categoryId
+              )}
+              onChange={(e) => handleCategoryChange(category, e.target.checked)}
+            />
+            {category.categoryName.charAt(0).toUpperCase() +
+              category.categoryName.slice(1)}
+          </label>
+        ))}
       </div>
       <label className="mt-3 flex flex-col font-secondary font-medium text-shark-950 text-[15px]">
         Upload Event Image* (Maximum image size is 100 MB)
@@ -190,25 +170,28 @@ export default function BasicInfoEC({ onValidityChange }: Props) {
           accept="image/*"
           onChange={(e) => {
             const file = e.target.files?.[0];
-            const maxSize = 100 * 1024 * 1024; // 100 MB
+            const maxSize = 100 * 1024 * 1024;
 
             if (file) {
               if (file.size > maxSize) {
-                setImagemessage(
+                setImageMessage(
                   'File size exceeds 100 MB. Please upload a smaller image.'
                 );
-                e.target.value = '';
-                setFile(null);
               } else {
-                setImagemessage('');
-                setFile(file);
+                setImageMessage('');
+                // You can upload the file and get a URL here
+                // Then store the URL in eventImageUrl
+                const imageUrl = URL.createObjectURL(file);
+                onChange({ eventImageUrl: imageUrl });
               }
             }
           }}
           className="mt-1 file:text-[15px] text-[14px] text-shark-950 file:mr-4 file:py-2 file:px-4 file:rounded-xl file:font-medium file:text-shark-950 file:border-0 file:bg-shark-200"
         />
         {imageMessage && (
-          <div className="mt-1 text-red-600 text-[13px] font-secondary font-normal">{imageMessage}</div>
+          <div className="mt-1 text-red-600 text-[13px] font-secondary font-normal">
+            {imageMessage}
+          </div>
         )}
       </label>
     </div>
