633fd0166c3ef0d76791d8a914e60930d7b42a0f
Ishan Hansaka <ishanhansakasilva@gmail.com>
2025-07-21 12:17:35 +0530

    chore: add more attributes

--- src/types/EventType.ts
+++ src/types/EventType.ts
@@ -14,5 +14,6 @@ export type EventType = {
   sponsorshipEnabled: boolean;
   donationEnabled: boolean;
   categories: { categoryId: number; categoryName: string }[];
-  organizer: string;
+  eventHostId: number;
+  organizationId: number;
 };
