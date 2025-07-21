a59b58b180d9e9820cc819bd7701915065cb5b4c
Ishan Hansaka <ishanhansakasilva@gmail.com>
2025-07-21 12:19:36 +0530

    chore: add event host modal

--- src/components/UI/VolunteerSidebar.tsx
+++ src/components/UI/VolunteerSidebar.tsx
@@ -1,6 +1,6 @@
-"use client";
+'use client';
 
-import { useState, useEffect } from "react";
+import { useState, useEffect } from 'react';
 import {
   Home,
   BarChart,
@@ -11,9 +11,10 @@ import {
   Settings,
   LogOut,
   LucideIcon,
-} from "lucide-react";
-import Link from "next/link";
-import { Button } from "@heroui/button";
+} from 'lucide-react';
+import Link from 'next/link';
+import { Button, useDisclosure } from '@heroui/react';
+import VolunteerToHostModal from './VolunteerToHostModal';
 
 interface MenuItem {
   name: string;
@@ -24,7 +25,8 @@ interface MenuItem {
 
 const VolunteerSidebar = () => {
   const [notificationCount, setNotificationCount] = useState<number>(0);
-  const [selectedItem, setSelectedItem] = useState<string>("Home");
+  const [selectedItem, setSelectedItem] = useState<string>('Home');
+  const { isOpen, onOpen, onOpenChange } = useDisclosure();
 
   useEffect(() => {
     setTimeout(() => {
@@ -33,18 +35,18 @@ const VolunteerSidebar = () => {
   }, []);
 
   const menuItems: MenuItem[] = [
-    { name: "Home", icon: Home, href: "/" },
-    { name: "Dashboard", icon: BarChart, href: "/Volunteer/dashboard" },
-    { name: "Profile", icon: User, href: "/Volunteer/profile" },
-    { name: "Events", icon: Calendar, href: "/Volunteer/events" },
+    { name: 'Home', icon: Home, href: '/' },
+    { name: 'Dashboard', icon: BarChart, href: '/Volunteer/dashboard' },
+    { name: 'Profile', icon: User, href: '/Volunteer/profile' },
+    { name: 'Events', icon: Calendar, href: '/Volunteer/events' },
     {
-      name: "Notifications",
+      name: 'Notifications',
       icon: Bell,
       badge: notificationCount,
-      href: "/notifications",
+      href: '/notifications',
     },
-    { name: "Social Feed", icon: Send, href: "/social-feed" },
-    { name: "Settings", icon: Settings, href: "/Volunteer/settings" },
+    { name: 'Social Feed', icon: Send, href: '/social-feed' },
+    { name: 'Settings', icon: Settings, href: '/Volunteer/settings' },
   ];
 
   return (
@@ -66,17 +68,17 @@ const VolunteerSidebar = () => {
               const isActive = selectedItem === item.name;
 
               return (
-                <Link key={item.name} href={item.href || "#"}>
+                <Link key={item.name} href={item.href || '#'}>
                   <div
                     onClick={() => setSelectedItem(item.name)}
                     className={`w-full cursor-pointer text-left flex items-center justify-between px-2 py-2 rounded-md hover:bg-verdant-50 relative ${
-                      isActive ? "text-verdant-700 font-semibold" : ""
+                      isActive ? 'text-verdant-700 font-semibold' : ''
                     }`}
                   >
                     <div className="flex items-center space-x-2">
                       <item.icon
                         className={`h-5 w-5 ${
-                          isActive ? "text-verdant-700" : ""
+                          isActive ? 'text-verdant-700' : ''
                         }`}
                       />
                       <span className="font-secondary font-medium text-shark-950">
@@ -84,7 +86,7 @@ const VolunteerSidebar = () => {
                       </span>
                     </div>
 
-                    {typeof item.badge === "number" && item.badge > 0 && (
+                    {typeof item.badge === 'number' && item.badge > 0 && (
                       <span className="text-xs bg-verdant-100 text-shark-950 px-1.5 rounded-md">
                         {item.badge}
                       </span>
@@ -99,15 +101,22 @@ const VolunteerSidebar = () => {
             })}
           </div>
         </nav>
-        <Button className="rounded-full bg-shark-950 text-shark-50 font-primary text-base tracking-wide mt-8">
+        <Button
+          onPress={onOpen}
+          className="rounded-full bg-shark-950 text-shark-50 font-primary text-base tracking-wide mt-8"
+        >
           Switch to Event Host
         </Button>
+        <VolunteerToHostModal
+          isOpen={isOpen}
+          onOpenChange={onOpenChange}
+        />
       </div>
 
       {/* Logout */}
       <div>
         <button
-          onClick={() => setSelectedItem("Logout")}
+          onClick={() => setSelectedItem('Logout')}
           className="flex items-center justify-between px-2 py-2 rounded-md hover:bg-shark-50 group text-sm text-shark-950 w-full text-left"
         >
           <div className="flex items-center space-x-2">
