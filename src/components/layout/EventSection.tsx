f18bf0ba94939d330cee5148ddb951d80abe2827
Ishan Hansaka <ishanhansakasilva@gmail.com>
2025-07-23 18:52:55 +0530

    feat: udpate to display dynamic data

--- src/components/layout/EventSection.tsx
+++ src/components/layout/EventSection.tsx
@@ -1,39 +1,42 @@
 'use client';
 
 import EventCard from '../UI/EventCard';
+import EventCardSkeleton from '../UI/EventCardSkeleton';
+import EventErrorDisplay from '../UI/EventErrorDisplay';
 import Image from 'next/image';
 import { EventType } from '@/types/EventType';
 import { useRouter } from 'next/navigation';
-
-const eventData: EventType = {
-  eventId: 1,
-  eventTitle: 'FIT Future Careers',
-  eventDescription:
-    'Join us for an exclusive event focused on connecting aspiring professionals with industry leaders.',
-  eventLocation: 'FIT Auditorium',
-  eventStartDate: '2025-11-20',
-  eventEndDate: '2025-11-22',
-  eventTime: '10:00:00',
-  eventImageUrl: '/images/DummyEvent2.png',
-  volunteerCount: 0,
-  eventType: 'ONLINE',
-  eventVisibility: 'PRIVATE',
-  eventStatus: 'PENDING',
-  sponsorshipEnabled: true,
-  donationEnabled: false,
-  categories: [
-    { categoryId: 1, categoryName: 'environment' },
-    { categoryId: 3, categoryName: 'technology' },
-  ],
-  organizer: 'INTECS, UoM',
-};
+import { useEffect, useState } from 'react';
+import { fetchAllEvents } from '@/services/eventService';
 
 export default function EventSection() {
+  const [events, setEvents] = useState<EventType[]>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [error, setError] = useState<string | null>(null);
+
   const router = useRouter();
+
+  useEffect(() => {
+    const getAllEvents = async () => {
+      try {
+        const eventsData = await fetchAllEvents();
+        setEvents(eventsData);
+        setError(null);
+      } catch (err) {
+        console.error('Error fetching all events:', err);
+        setError('Failed to fetch events.');
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    getAllEvents();
+  }, []);
 
   const handleNavigate = () => {
     router.push('/events');
   };
+
   return (
     <div className="w-full flex items-start justify-center mt-24">
       <div className="w-[1054px] flex flex-col justify-center text-center">
@@ -45,9 +48,26 @@ export default function EventSection() {
           planning. From local clean-ups to global initiatives, stay connected
           and never miss a chance to make an impact.
         </div>
+
         <div className="mt-16 flex flex-col gap-9">
+          {isLoading && (
+            <div className="flex gap-[65px]">
+              <EventCardSkeleton />
+              <EventCardSkeleton />
+              <EventCardSkeleton />
+            </div>
+          )}
+
+          {error && <EventErrorDisplay error={error} />}
+
           <div className="flex gap-[65px]">
-            <EventCard event={eventData} />
+            {!isLoading &&
+              !error &&
+              events
+                .slice(0, 3)
+                .map((event) => (
+                  <EventCard key={event.eventId} event={event} />
+                ))}
           </div>
           <div
             className="flex gap-1 justify-end cursor-pointer"
