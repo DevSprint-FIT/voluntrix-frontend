ab79f3ade568a5d9eddcd7d3d503fe1cc519cbec
Ishan Hansaka <ishanhansakasilva@gmail.com>
2025-07-23 12:28:23 +0530

    chore: add organization type and fix reciveing data json

--- src/services/organizationService.ts
+++ src/services/organizationService.ts
@@ -1,3 +1,4 @@
+import { OrganizationType } from '@/types/OrganizationType';
 import axios from 'axios';
 
 export const fetchOrganizationTitles = async () => {
@@ -13,13 +14,13 @@ export const fetchOrganizationTitles = async () => {
   }
 };
 
-export const fetchOrganizationById = async (id: number) => {
+export const fetchOrganizationById = async (id: number): Promise<OrganizationType> => {
   try {
     const response = await axios.get(
       `http://localhost:8080/api/public/organizations/${id}`
     );
 
-    return response.data;
+    return response.data.data;
   } catch (error) {
     console.error('Error fetching organization by ID:', error);
     throw error;
