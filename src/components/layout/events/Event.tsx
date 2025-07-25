eee61a053e414682bbeaccd70ed58bfd13f94c85
Ishan Hansaka <ishanhansakasilva@gmail.com>
2025-07-25 19:03:17 +0530

    feat: add sponsorship and donation enabled

--- src/components/layout/events/Event.tsx
+++ src/components/layout/events/Event.tsx
@@ -21,7 +21,6 @@ export default function Event({
   organization: OrganizationType | null;
 }) {
   const [isSaved, setIsSaved] = useState(false);
-  const [value] = useState(40);
 
   const handleSave = () => {
     setIsSaved((prevState) => !prevState);
@@ -58,7 +57,7 @@ export default function Event({
       <div className="w-[1200px] flex flex-col items-center justify-start">
         <div className="mt-14 flex gap-20">
           <div className="w-[522px] flex items-center justify-end">
-            <div className='w-[495px] h-[369px] relative overflow-hidden'>
+            <div className="w-[495px] h-[369px] relative overflow-hidden">
               <Image
                 src={event.eventImageUrl} //'/images/DummyEvent2.png'
                 width={495}
@@ -173,101 +172,110 @@ export default function Event({
             </div>
           </div>
         </div>
-        <div className="w-[1150px] flex gap-10 mt-20 justify-start">
-          <div className="w-[719px] flex flex-col gap-7 justify-start">
-            <p className="text-4xl font-secondary font-medium text-shark-950">
-              Sponsorships
-            </p>
-            <p className="w-[690px] text-shark-950 text-[16px] font-normal text-left text-wrap">
-              Excepteur efficient emerging, minim veniam anim aute carefully
-              curated Ginza conversation exquisite perfect nostrud nisi
-              intricate Content. Qui international first-class nulla ut.
-              Punctual adipisicing, essential lovely queen tempor eiusmod irure.
-              Excepteur...
-            </p>
-            <div className="flex items-center gap-2 flex-wrap">
-              {sponsor.sponsorships.map((tag, index) => (
-                <div
-                  key={index}
-                  className="flex h-[22px] px-2 justify-center items-center rounded-[4px] bg-[#E7E7E7]"
-                >
-                  <p className="text-[12px] font-primary font-bold text-shark-600">
-                    {tag}
-                  </p>
-                </div>
-              ))}
-            </div>
-            <Button
-              variant="ghost"
-              color="default"
-              className="w-[272px] h-[79px] flex items-center justify-center gap-3 rounded-[20px] border border-shark-600 bg-white"
-            >
-              <Image
-                src={'/icons/document.svg'}
-                width={40}
-                height={40}
-                alt="document"
-              />
-              <div className="flex flex-col">
-                <p className="font-medium text-shark-950 text-md">
-                  Sponsorship-proposal
-                </p>
-                <p className="font-normal text-shark-500 text-sm">
-                  0.17 MB PDF file
-                </p>
-              </div>
-            </Button>
-            <Button
-              variant="shadow"
-              className="w-[160px] bg-shark-950 text-white text-sm font-primary px-4 py-2 rounded-[20px] tracking-[1px]"
-            >
-              Sponsor Now
-            </Button>
-          </div>
-          {event.donationEnabled && (
-            <div className="w-[400px] flex flex-col gap-7 justify-start">
+        <div className="w-[1150px] flex gap-10 justify-start">
+          {event.sponsorshipEnabled && (
+            <div className="w-[719px] flex flex-col gap-7 justify-start mt-20">
               <p className="text-4xl font-secondary font-medium text-shark-950">
-                Donations
+                Sponsorships
               </p>
-              <p className="text-shark-950 text-[16px] font-normal text-left text-wrap">
+              <p className="w-[690px] text-shark-950 text-[16px] font-normal text-left text-wrap">
                 Excepteur efficient emerging, minim veniam anim aute carefully
                 curated Ginza conversation exquisite perfect nostrud nisi
                 intricate Content. Qui international first-class nulla ut.
                 Punctual adipisicing, essential lovely queen tempor eiusmod
                 irure. Excepteur...
               </p>
-              <div className="w-[380px] flex gap-1 justify-center items-center flex-col">
-                <Progress
-                  aria-label="Downloading..."
-                  className="w-[380px]"
-                  color="success"
-                  showValueLabel={false}
-                  size="md"
-                  value={value}
-                />
-                <div className="flex gap-1 justify-between w-full px-[4px] text-[16px] text-secondary font-medium">
-                  <div className="text-verdant-600">
-                    <span>{100 - value}%</span> to complete
+              <div className="flex items-center gap-2 flex-wrap">
+                {sponsor.sponsorships.map((tag, index) => (
+                  <div
+                    key={index}
+                    className="flex h-[22px] px-2 justify-center items-center rounded-[4px] bg-[#E7E7E7]"
+                  >
+                    <p className="text-[12px] font-primary font-bold text-shark-600">
+                      {tag}
+                    </p>
                   </div>
-                  <div className="text-shark-300">
-                    Goal $<span>20,000</span>
-                  </div>
-                </div>
+                ))}
               </div>
               <Button
-                variant="shadow"
-                className="flex gap-0 w-[160px] bg-verdant-800 text-white text-sm font-primary px-4 py-2 rounded-[20px] tracking-[1px]"
+                variant="ghost"
+                color="default"
+                className="w-[272px] h-[79px] flex items-center justify-center gap-3 rounded-[20px] border border-shark-600 bg-white"
+                onPress={() => {
+                  if (event.sponsorshipProposalUrl) {
+                    window.open(event.sponsorshipProposalUrl, '_blank');
+                  }
+                }}
               >
-                Donate Now
                 <Image
-                  src={'/icons/arrow-white-angel.svg'}
-                  width={32}
-                  height={32}
-                  alt="arrow"
+                  src={'/icons/document.svg'}
+                  width={40}
+                  height={40}
+                  alt="document"
                 />
+                <div className="flex flex-col">
+                  <p className="font-medium text-shark-950 text-md">
+                    Sponsorship-proposal
+                  </p>
+                </div>
+              </Button>
+              <Button
+                variant="shadow"
+                className="w-[160px] bg-shark-950 text-white text-sm font-primary px-4 py-2 rounded-[20px] tracking-[1px]"
+              >
+                Sponsor Now
               </Button>
             </div>
           )}
+          {event.donationEnabled &&
+            event.donations !== null &&
+            event.donationGoal !== null && (
+              <div className="w-[400px] flex flex-col gap-7 justify-start mt-20">
+                <p className="text-4xl font-secondary font-medium text-shark-950">
+                  Donations
+                </p>
+                <p className="text-shark-950 text-[16px] font-normal text-left text-wrap">
+                  Excepteur efficient emerging, minim veniam anim aute carefully
+                  curated Ginza conversation exquisite perfect nostrud nisi
+                  intricate Content. Qui international first-class nulla ut.
+                  Punctual adipisicing, essential lovely queen tempor eiusmod
+                  irure. Excepteur...
+                </p>
+                <div className="w-[380px] flex gap-1 justify-center items-center flex-col">
+                  <Progress
+                    aria-label="Downloading..."
+                    className="w-[380px]"
+                    color="success"
+                    showValueLabel={false}
+                    size="md"
+                    value={(event.donations / event.donationGoal) * 100}
+                  />
+                  <div className="flex gap-1 justify-between w-full px-[4px] text-[16px] text-secondary font-medium">
+                    <div className="text-verdant-600">
+                      <span>
+                        {100 - (event.donations / event.donationGoal) * 100}%
+                      </span>{' '}
+                      to complete
+                    </div>
+                    <div className="text-shark-300">
+                      Goal Rs.<span>{event.donationGoal}</span>
+                    </div>
+                  </div>
+                </div>
+                <Button
+                  variant="shadow"
+                  className="flex gap-0 w-[160px] bg-verdant-800 text-white text-sm font-primary px-4 py-2 rounded-[20px] tracking-[1px]"
+                >
+                  Donate Now
+                  <Image
+                    src={'/icons/arrow-white-angel.svg'}
+                    width={32}
+                    height={32}
+                    alt="arrow"
+                  />
+                </Button>
+              </div>
+            )}
         </div>
       </div>
     </div>
