e1fda5d255bd5401505eeb55324af4c206db6f2c
Ishan Hansaka <ishanhansakasilva@gmail.com>
2025-07-28 20:00:54 +0530

    feat: add dynamic routing

--- src/app/event-host/[id]/layout.tsx
+++ src/app/event-host/[id]/layout.tsx
@@ -3,12 +3,14 @@ import React from "react";
 
 export default function EventHostLayout({
   children,
+  params,
 }: {
   children: React.ReactNode;
+  params: { id: string };
 }) {
   return (
     <div className="flex">
-      <EventHostSidebar />
+      <EventHostSidebar hostId={params.id} />
       <div className="flex-1 p-6 ml-60">{children}</div>
     </div>
   );
