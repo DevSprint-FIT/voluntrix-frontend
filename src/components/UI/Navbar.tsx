4ed7e38d0ce3204108564bcbf8f4701bb85bda1f
Ishan Hansaka <ishanhansakasilva@gmail.com>
2025-08-01 15:30:29 +0530

    feat: check authenticate before access events

--- src/components/UI/Navbar.tsx
+++ src/components/UI/Navbar.tsx
@@ -1,12 +1,13 @@
-"use client";
+'use client';
 
-import Image from "next/image";
-import Link from "next/link";
-import { useEffect, useState } from "react";
-import { useRouter } from "next/navigation";
-import { gsap } from "gsap";
-import { ScrollTrigger } from "gsap/ScrollTrigger";
-import { Button } from "@heroui/button"; 
+import Image from 'next/image';
+import Link from 'next/link';
+import { useEffect, useState } from 'react';
+import { useRouter } from 'next/navigation';
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+import { Button } from '@heroui/button';
+import authService from '@/services/authService';
 
 gsap.registerPlugin(ScrollTrigger);
 
@@ -16,7 +17,7 @@ export default function Navbar() {
 
   useEffect(() => {
     const trigger = ScrollTrigger.create({
-      start: "top+=10px top",
+      start: 'top+=10px top',
       onEnter: () => setIsScrolled(true),
       onLeaveBack: () => setIsScrolled(false),
     });
@@ -24,19 +25,22 @@ export default function Navbar() {
     return () => trigger.kill();
   }, []);
 
-
   const login = () => {
-    router.push("/auth/login");
+    router.push('/auth/login');
   };
 
   const signup = () => {
-    router.push("/auth/signup");
+    router.push('/auth/signup');
   };
+
+  const eventPath = authService.isAuthenticated()
+    ? '/events'
+    : '/public/events';
 
   return (
     <nav
       className={`fixed z-50 top-0 flex items-center justify-between px-32 w-full transition-all duration-300 ease-in-out ${
-        isScrolled ? "shadow-md bg-white py-2" : "bg-transparent py-6"
+        isScrolled ? 'shadow-md bg-white py-2' : 'bg-transparent py-6'
       }`}
     >
       <div
@@ -44,31 +48,66 @@ export default function Navbar() {
       >
         <div
           className={`transition-all duration-300 ease-in-out ${
-            isScrolled ? "scale-90" : "scale-100"
+            isScrolled ? 'scale-90' : 'scale-100'
           }`}
         >
           <Link href="/">
-            <Image src="/images/logo.svg" alt="Voluntrix Logo" width={190} height={50} />
+            <Image
+              src="/images/logo.svg"
+              alt="Voluntrix Logo"
+              width={190}
+              height={50}
+            />
           </Link>
         </div>
 
         <div
           className={`flex space-x-16 text-shark-950 text-[1rem] font-primary tracking-wider font-medium transition-all duration-300 ease-in-out ${
-            isScrolled ? "text-[0.95rem]" : "text-[1.05rem]"
+            isScrolled ? 'text-[0.95rem]' : 'text-[1.05rem]'
           }`}
         >
-          <Link href="#" className="transition-all duration-300 ease-in-out hover:text-verdant-600">Features</Link>
-          <Link href="/events" className="transition-all duration-300 ease-in-out hover:text-verdant-600">Events</Link>
-          <Link href="/PublicFeed" className="transition-all duration-300 ease-in-out hover:text-verdant-600">Social Feed</Link>
-          <Link href="#" className="transition-all duration-300 ease-in-out hover:text-verdant-600">Volunteers</Link>
+          <Link
+            href="#"
+            className="transition-all duration-300 ease-in-out hover:text-verdant-600"
+          >
+            Features
+          </Link>
+          <Link
+            href={eventPath}
+            className="transition-all duration-300 ease-in-out hover:text-verdant-600"
+          >
+            Events
+          </Link>
+          <Link
+            href="/PublicFeed"
+            className="transition-all duration-300 ease-in-out hover:text-verdant-600"
+          >
+            Social Feed
+          </Link>
+          <Link
+            href="#"
+            className="transition-all duration-300 ease-in-out hover:text-verdant-600"
+          >
+            Volunteers
+          </Link>
         </div>
 
         {/* Login & Sign Up Buttons */}
         <div className="flex space-x-4 text-[1rem] transition-all duration-300 ease-in-out">
-          <Button onPress={login} variant="light" className="text-md text-shark-950 font-primary border-0 tracking-[1px] font-medium px-4 py-2 rounded-[20px]">
+          <Button
+            onPress={login}
+            variant="light"
+            className="text-md text-shark-950 font-primary border-0 tracking-[1px] font-medium px-4 py-2 rounded-[20px]"
+          >
             Login
           </Button>
-          <Button onPress={signup} variant="shadow" className="bg-shark-950 text-white text-sm font-primary px-4 py-2 rounded-[20px] tracking-[1px]">Sign Up</Button>
+          <Button
+            onPress={signup}
+            variant="shadow"
+            className="bg-shark-950 text-white text-sm font-primary px-4 py-2 rounded-[20px] tracking-[1px]"
+          >
+            Sign Up
+          </Button>
         </div>
       </div>
     </nav>
